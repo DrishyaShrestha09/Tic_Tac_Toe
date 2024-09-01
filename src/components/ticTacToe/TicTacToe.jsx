02d3e68854468ed6875e3886bc08c77be748b693
Drishya Shrestha <ridanstha09@gmail.com>
2024-09-01 12:04:18 +0545

    fixed reset btn

--- src/components/ticTacToe/TicTacToe.jsx
+++ src/components/ticTacToe/TicTacToe.jsx
@@ -5,12 +5,12 @@
 
 const TicTacToe = () => {
   return (
-    <div className="Container text-center bg-#0f1B21 ">
-        <h1 className="title mt-12 text-white text-[60px] ">Tic Tac Toe Game In <span>React</span></h1>
+    <div className="Container text-center">
+        <h1 className="title mt-12 text-white text-[60px] flex justify-center font-semibold">Tic Tac Toe Game In  <span className="text-[#26ffcb] pl-4 font-semibold"> React</span></h1>
         <div className="board">
 
         </div>
-        <button className="reset">Reset</button>
+        <button className="reset w-64 h-28 border-none outline-none cursor-pointer rounded-full bg-[#1f3540] text-2xl text-[#26ffcb] mt-6 mb-12 ">Reset</button>
     </div>
   )
 }
